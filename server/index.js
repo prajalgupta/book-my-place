aacee029aec159bf20b445faa0364b69ed1c2fbe
Prajal Gupta <gupta.prajal@gmail.com>
2025-03-28 09:17:50 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -30,9 +30,9 @@ const connectDB = () => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connected to Mongo DB"))
+    .then(() => console.log("Connected to MongoDB"))
     .catch((err) => {
-      console.error("Failed to connect with mongo");
+      console.error("Failed to connect with MongoDB");
       console.error(err);
     });
 };
