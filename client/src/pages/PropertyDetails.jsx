eddfff27dbb5ea5b0f90e5f08c2e6595c64b046c
Prajal Gupta <gupta.prajal@gmail.com>
2025-03-28 10:12:11 +0530

    import statement missing

--- client/src/pages/PropertyDetails.jsx
+++ client/src/pages/PropertyDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { DatePicker } from "@mui/lab";
 import { CircularProgress, Rating, TextField } from "@mui/material";
